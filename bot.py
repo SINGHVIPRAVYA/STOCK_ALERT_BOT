392b4e282225b83fb83769c926b1740aa92c3c7c
SINGHVIPRAVYA <singhvipravya@gmail.com>
2026-07-18 14:02:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,43 @@
+import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-BOT_TOKEN = "8875474288:AAHrwzJKbskhDABeH5P4x8Eh0VOI13Iy_X0"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "✅ STOCK ALERT BOT START HO GAYA.\n\n"
-        "Ab jaldi hi Volume, News, PDF, Bulk Deal aur Technical Alerts add honge."
-    )
+    text = """
+🤖 AI STOCK ASSISTANT
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+✅ Bot Successfully Started
 
-app.add_handler(CommandHandler("start", start))
+🚀 Coming Soon Features
 
-app.run_polling()
+📈 Volume Analysis
+📊 Bulk Deal
+📊 Block Deal
+📄 NSE/BSE PDF Alerts
+📰 AI Hindi News Summary
+📉 Support & Resistance
+📈 20/50/100/200 DMA
+📐 Chart Pattern Detection
+🔥 Breakout Alerts
+🌅 Morning Report
+🌙 Night Report
+⭐ Unlimited Watchlist
+⚙️ Settings Menu
+
+Bot is Online ✅
+"""
+    await update.message.reply_text(text)
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot Started Successfully...")
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
